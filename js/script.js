87d95559626135d8c7fb93d9a1a21063da1c7b65
Ilya Katuntsev <masfel13@gmail.com>
2017-12-08 19:15:18 +0500

    закрытие формы, валидация

--- js/script.js
+++ js/script.js
@@ -2,14 +2,34 @@ var callbackBtn = document.querySelector('.map-button');
 
 var popup = document.querySelector('.callback-form');
 
-var close = document.querySelector('.modal-close');
+var close = popup.querySelector('.modal-close');
+
+var form = popup.querySelector('form');
+var modalText = popup.querySelector('[name=textarea]');
+var modalEmail = popup.querySelector('[name=email]');
 
 callbackBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('modal-show');
+  modalName.focus();
 });
 
 close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('modal-show');
+});
+
+form.addEventListener('submit', function (evt) {
+  if (!modalText.value || !modalEmail.value) {
+  evt.preventDefault();
+  alert('Необходимо указать почту и написать вопрос');
+  }
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains('modal-show')) {
+      popup.classList.remove('modal-show');
+    };
+  };
 });
